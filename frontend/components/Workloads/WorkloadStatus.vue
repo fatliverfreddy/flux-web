d4bef189111c468340174b1a17425b62fcf7e558
Tzahi Argaman <argamanza@gmail.com>
2020-02-06 11:43:16 +0200

    add tooltip to workload status icon

--- frontend/components/Workloads/WorkloadStatus.vue
+++ frontend/components/Workloads/WorkloadStatus.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="icon" :style="iconStyle"></div>
+    <div class="icon tooltip" :style="iconStyle">
+      <span class="tooltiptext">{{ iconTooltip }}</span>
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,57 @@
   background-size: contain;
   width: 35px;
   height: 35px;
+}
+
+/* Tooltip container */
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+/* Tooltip text */
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 100px;
+  background-color: #2f395b;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+
+  /* Font Styling */
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 300;
+  font-size: 13px;
+
+  /* Position the tooltip text */
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -60px;
+
+  /* Fade in tooltip */
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+/* Tooltip arrow */
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Show the tooltip text when you mouse over the tooltip container */
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
 
@@ -33,6 +86,38 @@ export default class WorkloadStatus extends Vue {
     return {
       backgroundImage: `url(${require(`@/assets/images/${imageName}.${imageExtension}`)})`
     };
+  }
+
+  get iconTooltip() {
+    const status = this.getWorkload(this.workload).status;
+    var statusLiteral = "";
+
+    switch (status) {
+      case "up_to_date":
+        statusLiteral = "Up to Date";
+        break;
+
+      case "releasing":
+        statusLiteral = "Release Failed";
+        break;
+
+      case "behind":
+        statusLiteral = "Behind";
+        break;
+
+      case "unknown":
+        statusLiteral = "Unknown";
+        break;
+
+      case "release_failed":
+        statusLiteral = "Release Failed";
+        break;
+
+      default:
+        break;
+    }
+
+    return statusLiteral;
   }
 }
 </script>
